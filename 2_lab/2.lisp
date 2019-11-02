ce621e46cbabddd834b25065843402ccfb4b281a
Reywaltz <35903659+Reywaltz@users.noreply.github.com>
2019-11-02 11:49:15 +0300

    3_lab

--- 2_lab/2.lisp
+++ 2_lab/2.lisp
@@ -1,6 +1,7 @@
+(defun file_read()
 (let ((in (open "C:/Users/GeorgePC/Desktop/Учёба/Функциональное прог/lab_2/2_lab/file.txt" :if-does-not-exist nil)))
       (when in
             (loop for line = (read-line in nil)
             while line do (format t "~a~%" line))
-      (close in)))
-      
+      (close in))))
+(file_read)
